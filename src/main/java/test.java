c29917e791cd17ff7bb0a044ae8bd609071e7470
leowang <leowang@leowangs-MacBook-Air.local>
2018-03-06 14:30:41 +0800

    add something different

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,6 +6,7 @@ public class test {
     public static void main (String[] args) {
         C o1 = new C();
         o1.add(6);
+        String a = "Do something different";
     }
 
 }
--- src/test/java/testTest.java
+++ src/test/java/testTest.java
@@ -1,5 +1,20 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class testTest {
+
+    @Test
+    public void testAdd1() {
+        assertEquals (9, (new C()).add(6) );
+    }
+    @Test
+    public void testAdd2() {
+        assertEquals (-2, (new C()).add(-5) );
+    }
+    @Test
+    public void testAdd3() {
+        assertEquals (3, (new C()).add(0) );
+    }
 
 }
